7d389e23fdab90d2dde98aae6603a8a30932649e
yangqc <yangqc_ars@outlook.com>
2017-05-08 23:57:54 +0800

    groovy 变量

---
+++ src/main/groovy/com/yqc/dynamic/DynamicVariable.groovy
@@ -0,0 +1,20 @@
+package com.yqc.dynamic
+
+/**
+ * Created by yangqc on 2017/5/8.
+ */
+class DynamicVariable {
+    static void main(String[] args) {
+        def a = 100
+        println(a)
+
+        def b = 100.10
+        println(b)
+
+        def c = 100.101
+        println(c)
+
+        def d = "Hello,World"
+        println(d)
+    }
+}
---
+++ src/main/groovy/com/yqc/dynamic/VariableBestPractice.groovy
@@ -0,0 +1,21 @@
+package com.yqc.dynamic
+
+/**
+ * Created by yangqc on 2017/5/8.
+ * 针对变量命名的最佳实践
+ */
+class VariableBestPractice {
+    static void main(String[] args) {
+        def aint = 100
+        println(aint)
+
+        def bfloat = 100.10
+        println(bfloat)
+
+        def cDouble = 100.101
+        println(cDouble)
+
+        def dString = "Hello,World!"
+        println(dString)
+    }
+}
