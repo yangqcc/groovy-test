90c157c0ebee1401f67d147ecebba26cab81f439
yangqc <yangqc_ars@outlook.com>
2017-09-29 23:15:00 +0800

    整理

--- src/main/groovy/com/yqc/range/RangeTest1.groovy
+++ src/main/groovy/com/yqc/range/RangeTest1.groovy
@@ -7,15 +7,28 @@ class RangeTest1 {
     static void main(String[] args) {
         //°üº¬·¶Î§
         def rang1 = 1..10
+        println(rang1)
+
         //¶ÀÕ¼·¶Î§
         def range2 = 1..<10
+        for (x in range2) {
+            print(x)
+        }
+        println()
+
         //·¶Î§Ò²¿ÉÒÔ×Ö·û´®×é³É
         def rangeSequence = 'a'..'x'
         //·¶Î§Ò²¿ÉÒÔ½µÐòÅÅÁÐ
         def rangeDes = 10..1
         //·¶Î§Ò²¿ÉÒÔ×Ö·û´®½µÐòÅÅÁÐ
         def rangeSequenceDes = 'x'..'a'
+        println("½µÐò±éÀú×Ö·û´®")
+        for (x in rangeSequenceDes) {
+            print(x)
+        }
+        println()
 
+        println("ÉýÐò±éÀú×Ö·û´®")
         for (x in rangeSequence) {
             println(x)
         }
